f0b001ecca48b2ce9e14f694a0753e22ce7059f8
Vitoriano <vitorianoernandes2012@gmail.com>
2020-10-22 18:49:23 -0300

    Initial commit

---
+++ dist/types.js
@@ -0,0 +1,18 @@
+"use strict";
+// boolean (true / false)
+var isOpen;
+isOpen = true;
+// string ('foo', "foo", `foo`)
+var message;
+message = "foo " + isOpen;
+// number (int, float, hex, binary)
+var total;
+total = 0xff0;
+// array (type[])
+var items;
+items = [1, 2, 3];
+var values;
+values = [1, 2, 3, 4, 5];
+// tuple 
+var title;
+title = [1, 'Matrix'];
---
+++ src/types.ts
@@ -0,0 +1,50 @@
+// boolean (true / false)
+let isOpen: boolean
+isOpen = true
+
+// string ('foo', "foo", `foo`)
+let message: string
+message = `foo ${isOpen}`
+
+// number (int, float, hex, binary)
+let total: number
+total = 0xff0
+
+// array (type[])
+let items: number[]
+items = [1,2,3]
+
+let values : Array<number>
+values = [1,2,3,4,5]
+
+// tuple 
+let title: [number, string]
+title = [1, 'Matrix']
+
+// enum 
+enum Colors {
+  white = '#fff',
+  black = '#000'
+}
+
+// any (qualquer coisa) NÃO É LEGAL!
+let coisa: any
+coisa = true
+
+// void (vazio)
+
+function logger(): void{
+  console.log('log')
+}
+
+// null / undefined
+type Bla = string | undefined
+
+// never
+function error(): never {
+  throw new Error("error")
+}
+
+// object 
+let cart: object
+cart = { key: "fi" }
